78054c7f997e2f3327178090f047f75f85287d40
Lukáš Valenta <valenta@lvalenta.cz>
2023-04-04 22:22:51 +0200

    RouterCoordinator fix

--- Sources/CleevioRouters/Coordinators/RouterCoordinator.swift
+++ Sources/CleevioRouters/Coordinators/RouterCoordinator.swift
@@ -15,21 +15,22 @@ public protocol RouterEventDelegate: CoordinatorEventDelegate {
 
 @available(macOS 10.15, *)
 open class RouterCoordinator<RouterType: Router>: Coordinator, RouterEventDelegate {
-    final let router: RouterType
-    final let animated: Bool
-
+    final public var router: RouterType
+    final public var animated: Bool
+    
     private weak var delegate: RouterEventDelegate?
+    private let cancelBag = CancelBag()
 
     open func setDelegate(_ delegate: some RouterEventDelegate) {
         self.delegate = delegate
     }
-
+    
     public init(router: RouterType, animated: Bool) {
         self.router = router
         self.animated = animated
         super.init()
     }
-
+    
     open override func onDeinit(of coordinator: Coordinator) {
         super.onDeinit(of: coordinator)
         delegate?.onDeinit(of: coordinator)
@@ -44,5 +45,18 @@ open class RouterCoordinator<RouterType: Router>: Coordinator, RouterEventDelega
     open func onDismissedByRouter(of coordinator: CleevioCore.Coordinator) {
         removeChildCoordinator(coordinator)
         delegate?.onDismissedByRouter(of: coordinator)
+    }
+    
+    open func present<T: DismissHandler & PlatformViewController>(viewController: T) {
+        self.viewController = viewController
+
+        viewController.dismissPublisher
+            .sink(receiveValue: { [weak self] in
+                guard let self else { return }
+                self.delegate?.onDismissedByRouter(of: self)
+            })
+            .store(in: cancelBag)
+
+        router.present(viewController, animated: animated)
     }
 }
