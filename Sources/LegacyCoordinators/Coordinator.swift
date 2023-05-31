1944ac434d9affdabadb5817257e66d770fac906
Lukas Valenta <lukas.valenta@cleevio.com>
2023-05-31 23:53:22 +0000

    LegacyCoordinators

---
+++ Sources/LegacyCoordinators/Coordinator.swift
@@ -0,0 +1,26 @@
+//
+//  Coordinator.swift
+//  CleevioUIExample
+//
+//  Created by Thành Đỗ Long on 14.01.2022.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import CleevioRouters
+import Combine
+
+@available(iOS 13.0, macOS 10.15, *)
+public typealias CoordinatingResult<T> = AnyPublisher<T, Never>
+@available(iOS 13.0, macOS 10.15, *)
+public typealias CoordinatingSubject<T> = PassthroughSubject<T, Never>
+
+@available(iOS 13.0, macOS 10.15, *)
+@available(*, deprecated, message: "It is expected to use new coordinators from now on")
+public protocol LegacyCoordinator {
+    associatedtype CoordinationResult
+
+    var identifier: UUID { get }
+
+    func start() -> CoordinatingResult<CoordinationResult>
+}
