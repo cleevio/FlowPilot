1e09e986039f3c50a5fd0be187659d99e3f05d05
Lukáš Valenta <valenta@lvalenta.cz>
2024-11-14 23:27:40 +0100

    Updated example project

--- Example/Sources/Scenes/Root/RootViewModel.swift
+++ Example/Sources/Scenes/Root/RootViewModel.swift
@@ -8,13 +8,40 @@
 import Foundation
 import Combine
 
+protocol RootViewModelRoutingDelegate: AnyObject {
+    func showFirst() async throws
+    func showSecondView() async throws
+    func showThirdModal() async throws
+    func dismiss() async
+    func valueSelection(initial: Bool) async throws -> Bool
+}
+
+@MainActor
 final class RootViewModel: ObservableObject {
-    var route: PassthroughSubject<Route, Never> = .init()
-    
-    enum Route {
+    enum Action {
         case showFirst
         case showSecond
         case showThirdModal
         case dismiss
+        case valueSelectionTap
+    }
+
+    weak var routingDelegate: RootViewModelRoutingDelegate?
+
+    @Published var selectedValue: Bool?
+
+    func send(action: Action) async {
+        switch action {
+        case .showFirst:
+            try? await routingDelegate?.showFirst()
+        case .showSecond:
+            try? await routingDelegate?.showSecondView()
+        case .showThirdModal:
+            try? await routingDelegate?.showThirdModal()
+        case .dismiss:
+            await routingDelegate?.dismiss()
+        case .valueSelectionTap:
+            selectedValue = try? await routingDelegate?.valueSelection(initial: selectedValue ?? false)
+        }
     }
 }
