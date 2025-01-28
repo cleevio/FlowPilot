66019a6c91ab5c6cf3cb8104e4d7cd19c68048bd
Lukáš Valenta <valenta@lvalenta.cz>
2025-01-28 15:39:09 +0100

    Support Swift 5.5

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -1,9 +1,16 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let swiftSettings: [SwiftSetting] = [ ]
+let swiftSettings: [SwiftSetting] = [
+    // Use for development to catch concurrency issues. SPM packages cannot depend on other packages that use unsafeFlags.
+//    SwiftSetting.unsafeFlags([
+//        "-Xfrontend", "-strict-concurrency=complete",
+//        "-Xfrontend", "-warn-concurrency",
+//        "-Xfrontend", "-enable-actor-data-race-checks",
+//    ])
+]
 
 let package = Package(
     name: "FlowPilot",
